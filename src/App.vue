68c25e283ae6af1d5c281a1cf8e9f251ac7b291d
saraSchulz <saraschulz2805@gmail.com>
2025-08-22 11:58:58 -0300

    ajustes dos icones importação dos componentes correta

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,26 @@ import FooterComponent from './components/FooterComponent.vue';
 import BannerSobreNosComponent from '@/components/sobreNos/BannerSobreNosComponent.vue';
 import ObjetivoComponent from '@/components/sobreNos/objetivoComponent.vue';
 import equipeComponent from '@/components/sobreNos/equipeComponent.vue';
-/*import BannerComponent  from '@/components/BannerComponent.vue';
-import SelectComponent from '@/components/SelectComponent.vue';
-import ClimaHojeComponent from '@/components/ClimaHojeComponent.vue';
-import SobreComponent from '@/components/SobreComponent.vue';
-*/
+import BannerComponent  from '@/components/home/BannerHomeComponent.vue';
+import SelectComponent from '@/components/home/SelectComponent.vue';
+import ClimaHojeComponent from '@/components/home/ClimaHojeComponent.vue';
+import SobreComponent from '@/components/home/SobreComponent.vue';
+/**/
 </script>
 <template>
   <header>
     <HeaderComponent/>
   </header>
   <main>
-    <BannerSobreNosComponent/>
-    <ObjetivoComponent/>
-    <equipeComponent/>
-    <!--
     <BannerComponent/>
     <SelectComponent/>
     <ClimaHojeComponent/>
     <SobreComponent/>
-    -->
 
+    <BannerSobreNosComponent/>
+    <ObjetivoComponent/>
+    <equipeComponent/>
+    <!---->
   </main>
   <footer>
     <FooterComponent/>
