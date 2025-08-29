1e4b9e831ec6c839f773345f7d7863a03b0280c6
saraSchulz <saraschulz2805@gmail.com>
2025-08-29 11:57:00 -0300

    Paginas de previsões novas img de background comentario no app.vue para teste

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 /*Header/Footer*/
 import HeaderComponent from '@/components/HeaderComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
-
+import FooterComponent from '@/components/FooterComponent.vue';
+import ClimasFrioComponent from '@/components/climaAtual/ClimasFrioComponent.vue';
+import ClimasComponent from '@/components/climaAtual/ClimasComponent.vue';
+import ClimasQuenteComponent from '@/components/climaAtual/ClimasQuenteComponent.vue';
 </script>
 <template>
 
     <HeaderComponent/>
   <main>
-
-    <RouterView />
+    <ClimasFrioComponent/>
+    <ClimasComponent/>
+    <ClimasQuenteComponent/>
+    <!-- <RouterView /> -->
   </main>
     <FooterComponent/>
     <!---->
