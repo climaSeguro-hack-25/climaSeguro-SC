0defbb8fe3a432e7f54839f9b328b5134f18115b
saraSchulz <sara.ifc2024@gmail.com>
2025-09-10 18:19:50 -0300

    ajustes do style

--- src/components/alerta/AlertaComponent.vue
+++ src/components/alerta/AlertaComponent.vue
@@ -3,7 +3,7 @@
 <template>
   <section class="alerta">
     <div class="texto">
-      <h2>
+      <h2 class="title">
         gostarias de receber nossos alertas?
       </h2>
       <div class="botoes">
@@ -25,24 +25,63 @@
   align-items: center;
   padding: 2rem;
   gap: 2rem;
-}
 
-&.texto {
+
+& .texto {
   max-width: 600px;
   color: #000;
 
+  & .title {
+    font-size: 60px;
+    transition: transform 1s ease;
+    display: inline-block;
+    text-shadow:  4px 4px 10px rgba(0, 0, 0, 0.8);
+    padding: 20px;
+    border-radius: 20px;
+    margin: 10px;
+    transition: all 1s ease;
+  }
+  & .title:hover{
+    transform: scale(1.1);
+    box-shadow: 4px 4px 30px rgba(0, 0, 0, 0.8);
+  }
+  & .botoes{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+      align-items: center;
+    & button{
+      background: none;
+      border: none;
+      font-size: 30px;
+      font-family: 'Julius Sans One';
+      font-weight: bold;
+      transition: all 0.8s ease;
+      padding: 10px;
+      border-radius: 10px;
+    }
+    & .sim:hover{
+      background: rgba(0, 0, 0, 0.35);
+      color: #ffffff;
+    transform: scale(1.1);
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.8);
+    }
+    & .nao{
+      color: #A20000;
 
-  & h2 {
-    font-size: 2.5rem;
-    line-height: 1.3;
-    margin-bottom: 2rem;
+    }
+    .nao:hover{
+      background: rgba(0, 0, 0, 0.35);
+color: #FAD7A0;
+    transform: scale(1.1);
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.8);
+    }
   }
 
 }
-
-.icone img {
-  width: 200px;
-  height: auto;
-  filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.3));
+  & .icone img {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
